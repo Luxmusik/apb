66130066aa97c0658ce25a721c5665eea797b20a
Ali Mazloum <79036904+alisscco@users.noreply.github.com>
2021-05-28 09:41:42 +0300

    Initialize dependabot/npm_and_yarn/modules/docs/app/VoloDocs.Web/jquery-validation-1.19.3

---
+++ templates/app/aspnet-core/src/MyCompanyName.MyProjectName.MongoDB/MongoDb/MyProjectNameMongoDbModule.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Volo.Abp.AuditLogging.MongoDB;
+using Volo.Abp.BackgroundJobs.MongoDB;
+using Volo.Abp.FeatureManagement.MongoDB;
+using Volo.Abp.Identity.MongoDB;
+using Volo.Abp.IdentityServer.MongoDB;
+using Volo.Abp.Modularity;
+using Volo.Abp.PermissionManagement.MongoDB;
+using Volo.Abp.SettingManagement.MongoDB;
+using Volo.Abp.TenantManagement.MongoDB;
+using Volo.Abp.Uow;
+//<TEMPLATE-REMOVE IF-NOT='CMS-KIT'>
+using Volo.CmsKit.MongoDB;
+//</TEMPLATE-REMOVE>
+
+namespace MyCompanyName.MyProjectName.MongoDB
+{
+    [DependsOn(
+        typeof(MyProjectNameDomainModule),
+        typeof(AbpPermissionManagementMongoDbModule),
+        typeof(AbpSettingManagementMongoDbModule),
+        typeof(AbpIdentityMongoDbModule),
+        typeof(AbpIdentityServerMongoDbModule),
+        typeof(AbpBackgroundJobsMongoDbModule),
+        typeof(AbpAuditLoggingMongoDbModule),
+        typeof(AbpTenantManagementMongoDbModule),
+        //<TEMPLATE-REMOVE IF-NOT='CMS-KIT'>
+        typeof(CmsKitMongoDbModule),
+        //</TEMPLATE-REMOVE>
+        typeof(AbpFeatureManagementMongoDbModule)
+        )]
+    public class MyProjectNameMongoDbModule : AbpModule
+    {
+        public override void ConfigureServices(ServiceConfigurationContext context)
+        {
+            context.Services.AddMongoDbContext<MyProjectNameMongoDbContext>(options =>
+            {
+                options.AddDefaultRepositories();
+            });
+
+            Configure<AbpUnitOfWorkDefaultOptions>(options =>
+            {
+                options.TransactionBehavior = UnitOfWorkTransactionBehavior.Disabled;
+            });
+        }
+    }
+}
