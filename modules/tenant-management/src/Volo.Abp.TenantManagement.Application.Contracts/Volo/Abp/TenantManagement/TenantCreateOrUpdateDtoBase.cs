c36e9d23fb2b67a746801f5c3a1aa19487c8cdf3
Ali Mazloum <79036904+alisscco@users.noreply.github.com>
2021-05-28 09:37:46 +0300

    Initialize rel-2.3

---
+++ modules/tenant-management/src/Volo.Abp.TenantManagement.Application.Contracts/Volo/Abp/TenantManagement/TenantCreateOrUpdateDtoBase.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Volo.Abp.TenantManagement
+{
+    public abstract class TenantCreateOrUpdateDtoBase
+    {
+        [Required]
+        [StringLength(TenantConsts.MaxNameLength)]
+        public string Name { get; set; }
+    }
+}
