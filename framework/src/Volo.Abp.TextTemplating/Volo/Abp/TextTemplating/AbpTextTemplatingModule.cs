4043d1b6a9ccbf64588f3c22b8e10c1a4223147c
Ali Mazloum <79036904+alisscco@users.noreply.github.com>
2021-05-28 09:47:39 +0300

    Initialize stsrki/dev-datagrid-improvements

---
+++ framework/src/Volo.Abp.TextTemplating/Volo/Abp/TextTemplating/AbpTextTemplatingModule.cs
@@ -0,0 +1,13 @@
+﻿using Volo.Abp.Modularity;
+using Volo.Abp.TextTemplating.Scriban;
+
+namespace Volo.Abp.TextTemplating
+{
+    [DependsOn(
+        typeof(AbpTextTemplatingScribanModule)
+    )]
+    public class AbpTextTemplatingModule : AbpModule
+    {
+
+    }
+}
