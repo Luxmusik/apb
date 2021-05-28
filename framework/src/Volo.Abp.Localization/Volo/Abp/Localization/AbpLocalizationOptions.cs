aacdc3e2a730c07bc90daabde5679de182d62bfe
Ali Mazloum <79036904+alisscco@users.noreply.github.com>
2021-05-28 09:37:54 +0300

    Initialize rel-2.9

---
+++ framework/src/Volo.Abp.Localization/Volo/Abp/Localization/AbpLocalizationOptions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using Volo.Abp.Collections;
+
+namespace Volo.Abp.Localization
+{
+    public class AbpLocalizationOptions
+    {
+        public LocalizationResourceDictionary Resources { get; }
+
+        /// <summary>
+        /// Used as the default resource when resource was not specified on a localization operation.
+        /// </summary>
+        public Type DefaultResourceType { get; set; }
+
+        public ITypeList<ILocalizationResourceContributor> GlobalContributors { get; }
+
+        public List<LanguageInfo> Languages { get; }
+
+        public AbpLocalizationOptions()
+        {
+            Resources = new LocalizationResourceDictionary();
+            GlobalContributors = new TypeList<ILocalizationResourceContributor>();
+            Languages = new List<LanguageInfo>();
+        }
+    }
+}
