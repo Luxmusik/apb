f458afb0b4ec20f7d7c3b9c7311b9aace1d6140d
Ali Mazloum <79036904+alisscco@users.noreply.github.com>
2021-05-28 09:48:08 +0300

    Initialize stsrki/dev-samples

---
+++ framework/test/Volo.Abp.TextTemplating.Tests/Volo/Abp/TextTemplating/TestTemplateDefinitionProvider.cs
@@ -0,0 +1,40 @@
+﻿using Volo.Abp.TextTemplating.Localization;
+
+namespace Volo.Abp.TextTemplating
+{
+    public class TestTemplateDefinitionProvider : TemplateDefinitionProvider
+    {
+        public override void Define(ITemplateDefinitionContext context)
+        {
+            context.Add(
+                new TemplateDefinition(
+                    TestTemplates.WelcomeEmail,
+                    defaultCultureName: "en"
+                )
+            );
+
+            context.Add(
+                new TemplateDefinition(
+                    TestTemplates.ForgotPasswordEmail,
+                    localizationResource: typeof(TestLocalizationSource),
+                    layout: TestTemplates.TestTemplateLayout1
+                )
+            );
+
+            context.Add(
+                new TemplateDefinition(
+                    TestTemplates.TestTemplateLayout1,
+                    isLayout: true
+                )
+            );
+
+            context.Add(
+                new TemplateDefinition(
+                    TestTemplates.ShowDecimalNumber,
+                    localizationResource: typeof(TestLocalizationSource),
+                    layout: TestTemplates.TestTemplateLayout1
+                )
+            );
+        }
+    }
+}
