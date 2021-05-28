49a3277c9cb5a53a615d216cd92ab2351dbd50bc
Ali Mazloum <79036904+alisscco@users.noreply.github.com>
2021-05-28 09:41:29 +0300

    Initialize dependabot/npm_and_yarn/framework/test/Volo.Abp.AspNetCore.Mvc.UI.Bootstrap.Demo/jquery-validation-1.19.3

---
+++ modules/docs/src/Volo.Docs.Admin.Web/Navigation/DocsMenuContributor.cs
@@ -0,0 +1,36 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Localization;
+using Volo.Abp.UI.Navigation;
+using Volo.Docs.Localization;
+
+namespace Volo.Docs.Admin.Navigation
+{
+    public class DocsMenuContributor : IMenuContributor
+    {
+        public async Task ConfigureMenuAsync(MenuConfigurationContext context)
+        {
+            if (context.Menu.Name == StandardMenus.Main)
+            {
+                await ConfigureMainMenuAsync(context);
+            }
+        }
+
+        private Task ConfigureMainMenuAsync(MenuConfigurationContext context)
+        {
+            var administrationMenu = context.Menu.GetAdministration();
+
+            var l = context.GetLocalizer<DocsResource>();
+
+            var rootMenuItem = new ApplicationMenuItem(DocsMenuNames.GroupName, l["Menu:Documents"], icon: "fa fa-book");
+
+            administrationMenu.AddItem(rootMenuItem);
+
+            rootMenuItem.AddItem(new ApplicationMenuItem(DocsMenuNames.Projects, l["Menu:ProjectManagement"], "~/Docs/Admin/Projects", requiredPermissionName: DocsAdminPermissions.Projects.Default));
+            rootMenuItem.AddItem(new ApplicationMenuItem(DocsMenuNames.Documents, l["Menu:DocumentManagement"], "~/Docs/Admin/Documents", requiredPermissionName: DocsAdminPermissions.Documents.Default));
+
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ templates/app/aspnet-core/src/MyCompanyName.MyProjectName.Domain.Shared/MyProjectNameDomainSharedModule.cs
@@ -0,0 +1,65 @@
+﻿using MyCompanyName.MyProjectName.Localization;
+using Volo.Abp.AuditLogging;
+using Volo.Abp.BackgroundJobs;
+using Volo.Abp.FeatureManagement;
+using Volo.Abp.Identity;
+using Volo.Abp.IdentityServer;
+using Volo.Abp.Localization;
+using Volo.Abp.Localization.ExceptionHandling;
+using Volo.Abp.Modularity;
+using Volo.Abp.PermissionManagement;
+using Volo.Abp.SettingManagement;
+using Volo.Abp.TenantManagement;
+using Volo.Abp.Validation.Localization;
+using Volo.Abp.VirtualFileSystem;
+//<TEMPLATE-REMOVE IF-NOT='CMS-KIT'>
+using Volo.CmsKit;
+//</TEMPLATE-REMOVE>
+
+namespace MyCompanyName.MyProjectName
+{
+    [DependsOn(
+        typeof(AbpAuditLoggingDomainSharedModule),
+        typeof(AbpBackgroundJobsDomainSharedModule),
+        typeof(AbpFeatureManagementDomainSharedModule),
+        typeof(AbpIdentityDomainSharedModule),
+        typeof(AbpIdentityServerDomainSharedModule),
+        typeof(AbpPermissionManagementDomainSharedModule),
+        typeof(AbpSettingManagementDomainSharedModule),
+        //<TEMPLATE-REMOVE IF-NOT='CMS-KIT'>
+        typeof(CmsKitDomainSharedModule),
+        //</TEMPLATE-REMOVE>
+        typeof(AbpTenantManagementDomainSharedModule)
+        )]
+    public class MyProjectNameDomainSharedModule : AbpModule
+    {
+        public override void PreConfigureServices(ServiceConfigurationContext context)
+        {
+            MyProjectNameGlobalFeatureConfigurator.Configure();
+            MyProjectNameModuleExtensionConfigurator.Configure();
+        }
+
+        public override void ConfigureServices(ServiceConfigurationContext context)
+        {
+            Configure<AbpVirtualFileSystemOptions>(options =>
+            {
+                options.FileSets.AddEmbedded<MyProjectNameDomainSharedModule>();
+            });
+
+            Configure<AbpLocalizationOptions>(options =>
+            {
+                options.Resources
+                    .Add<MyProjectNameResource>("en")
+                    .AddBaseTypes(typeof(AbpValidationResource))
+                    .AddVirtualJson("/Localization/MyProjectName");
+
+                options.DefaultResourceType = typeof(MyProjectNameResource);
+            });
+
+            Configure<AbpExceptionLocalizationOptions>(options =>
+            {
+                options.MapCodeNamespace("MyProjectName", typeof(MyProjectNameResource));
+            });
+        }
+    }
+}
