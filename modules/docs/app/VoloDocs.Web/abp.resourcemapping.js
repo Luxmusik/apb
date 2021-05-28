a1e9c96989ae328280320af7e80127a632fbccc5
Ali Mazloum <79036904+alisscco@users.noreply.github.com>
2021-05-28 09:53:32 +0300

    Initialize document-index-backgroundjob

---
+++ modules/docs/app/VoloDocs.Web/abp.resourcemapping.js
@@ -0,0 +1,13 @@
+﻿module.exports = {
+    aliases: {
+        "@node_modules": "./node_modules",
+        "@libs": "./wwwroot/libs"
+    },
+    clean: [
+        "@libs"
+    ],
+    mappings: {
+        "@node_modules/@microsoft/signalr/dist/browser/signalr.js": "@libs/signalr/",
+        "@node_modules/@microsoft/signalr/dist/browser/signalr.js.map": "@libs/signalr/"
+    }
+};
---
+++ npm/ng-packs/packages/theme-shared/src/lib/handlers/index.ts
@@ -0,0 +1,3 @@
+export * from './error.handler';
+export * from './lazy-style.handler';
+export * from './datepicker-config';
