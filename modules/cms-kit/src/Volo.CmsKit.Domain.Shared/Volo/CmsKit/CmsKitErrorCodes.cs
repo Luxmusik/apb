58fde5b004f145020984f544c8ecd073ed1e2180
Ali Mazloum <79036904+alisscco@users.noreply.github.com>
2021-05-28 09:40:14 +0300

    Initialize maliming/scoped-AbpLazyServiceProvider

---
+++ modules/cms-kit/src/Volo.CmsKit.Domain.Shared/Volo/CmsKit/CmsKitErrorCodes.cs
@@ -0,0 +1,14 @@
+﻿namespace Volo.CmsKit
+{
+    public static class CmsKitErrorCodes
+    {
+        public const string TagAlreadyExist = "CmsKit:0001";
+        
+        public const string ContentAlreadyExist = "CmsKit:0002";
+        
+        public static class Pages
+        {
+            public const string UrlAlreadyExist = "CmsKit:Page:0001";
+        }
+    }
+}
