7d8883e774f12ec4dafe25053690301af82356f3
Ali Mazloum <79036904+alisscco@users.noreply.github.com>
2021-05-28 09:54:48 +0300

    Initialize rel-4.3

---
+++ framework/src/Volo.Abp.AspNetCore.Mvc/Volo/Abp/AspNetCore/Mvc/AbpAspNetCoreMvcOptions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using Volo.Abp.AspNetCore.Mvc.Conventions;
+
+namespace Volo.Abp.AspNetCore.Mvc
+{
+    public class AbpAspNetCoreMvcOptions
+    {
+        public bool? MinifyGeneratedScript { get; set; }
+
+        public AbpConventionalControllerOptions ConventionalControllers { get; }
+
+        public HashSet<Type> IgnoredControllersOnModelExclusion { get; }
+
+        public AbpAspNetCoreMvcOptions()
+        {
+            ConventionalControllers = new AbpConventionalControllerOptions();
+            IgnoredControllersOnModelExclusion = new HashSet<Type>();
+        }
+    }
+}
