7daecdec327b3345fb2f070a725e3b6b854a18db
Ali Mazloum <79036904+alisscco@users.noreply.github.com>
2021-05-28 09:43:04 +0300

    Initialize dependabot/npm_and_yarn/npm/packs/aspnetcore.mvc.ui.theme.shared/datatables.net-1.10.24

---
+++ templates/app/aspnet-core/src/MyCompanyName.MyProjectName.Application.Contracts/MyProjectNameApplicationContractsModule.cs
@@ -0,0 +1,35 @@
+﻿using Volo.Abp.Account;
+using Volo.Abp.FeatureManagement;
+using Volo.Abp.Identity;
+using Volo.Abp.Modularity;
+using Volo.Abp.ObjectExtending;
+using Volo.Abp.PermissionManagement;
+using Volo.Abp.SettingManagement;
+using Volo.Abp.TenantManagement;
+//<TEMPLATE-REMOVE IF-NOT='CMS-KIT'>
+using Volo.CmsKit.Admin;
+//</TEMPLATE-REMOVE>
+
+namespace MyCompanyName.MyProjectName
+{
+    [DependsOn(
+        typeof(MyProjectNameDomainSharedModule),
+        typeof(AbpAccountApplicationContractsModule),
+        typeof(AbpFeatureManagementApplicationContractsModule),
+        typeof(AbpIdentityApplicationContractsModule),
+        typeof(AbpPermissionManagementApplicationContractsModule),
+        typeof(AbpSettingManagementApplicationContractsModule),
+        typeof(AbpTenantManagementApplicationContractsModule),
+        //<TEMPLATE-REMOVE IF-NOT='CMS-KIT'>
+        typeof(CmsKitAdminApplicationContractsModule),
+        //</TEMPLATE-REMOVE>
+        typeof(AbpObjectExtendingModule)
+    )]
+    public class MyProjectNameApplicationContractsModule : AbpModule
+    {
+        public override void PreConfigureServices(ServiceConfigurationContext context)
+        {
+            MyProjectNameDtoExtensions.Configure();
+        }
+    }
+}
