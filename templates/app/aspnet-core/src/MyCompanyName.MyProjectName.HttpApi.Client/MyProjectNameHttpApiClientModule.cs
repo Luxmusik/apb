bfbb8d0d09438532f0dd5066cbcee815e60b8336
Ali Mazloum <79036904+alisscco@users.noreply.github.com>
2021-05-28 09:42:37 +0300

    Initialize dependabot/npm_and_yarn/modules/blogging/app/Volo.BloggingTestApp/jquery-validation-1.19.3

---
+++ templates/app/aspnet-core/src/MyCompanyName.MyProjectName.HttpApi.Client/MyProjectNameHttpApiClientModule.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Volo.Abp.Account;
+using Volo.Abp.FeatureManagement;
+using Volo.Abp.Identity;
+using Volo.Abp.Modularity;
+using Volo.Abp.PermissionManagement;
+using Volo.Abp.TenantManagement;
+using Volo.Abp.SettingManagement;
+//<TEMPLATE-REMOVE IF-NOT='CMS-KIT'>
+using Volo.CmsKit;
+//</TEMPLATE-REMOVE>
+
+namespace MyCompanyName.MyProjectName
+{
+    [DependsOn(
+        typeof(MyProjectNameApplicationContractsModule),
+        typeof(AbpAccountHttpApiClientModule),
+        typeof(AbpIdentityHttpApiClientModule),
+        typeof(AbpPermissionManagementHttpApiClientModule),
+        typeof(AbpTenantManagementHttpApiClientModule),
+        typeof(AbpFeatureManagementHttpApiClientModule),
+        //<TEMPLATE-REMOVE IF-NOT='CMS-KIT'>
+        typeof(CmsKitHttpApiClientModule),
+        //</TEMPLATE-REMOVE>
+        typeof(AbpSettingManagementHttpApiClientModule)
+    )]
+    public class MyProjectNameHttpApiClientModule : AbpModule
+    {
+        public const string RemoteServiceName = "Default";
+
+        public override void ConfigureServices(ServiceConfigurationContext context)
+        {
+            context.Services.AddHttpClientProxies(
+                typeof(MyProjectNameApplicationContractsModule).Assembly,
+                RemoteServiceName
+            );
+        }
+    }
+}
