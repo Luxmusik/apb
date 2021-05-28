29f22316708d015ca94dbf5bd572ee57b6f543ab
Ali Mazloum <79036904+alisscco@users.noreply.github.com>
2021-05-28 09:40:21 +0300

    Initialize rel-4.1

---
+++ framework/src/Volo.Abp.EntityFrameworkCore/Volo/Abp/Domain/Repositories/EfCoreRepositoryExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Volo.Abp.Domain.Entities;
+using Volo.Abp.Domain.Repositories.EntityFrameworkCore;
+
+namespace Volo.Abp.Domain.Repositories
+{
+    public static class EfCoreRepositoryExtensions
+    {
+        public static DbContext GetDbContext<TEntity>(this IReadOnlyBasicRepository<TEntity> repository)
+            where TEntity : class, IEntity
+        {
+            return repository.ToEfCoreRepository().DbContext;
+        }
+
+        public static DbSet<TEntity> GetDbSet<TEntity>(this IReadOnlyBasicRepository<TEntity> repository)
+            where TEntity : class, IEntity
+        {
+            return repository.ToEfCoreRepository().DbSet;
+        }
+
+        public static IEfCoreRepository<TEntity> ToEfCoreRepository<TEntity>(this IReadOnlyBasicRepository<TEntity> repository)
+            where TEntity : class, IEntity
+        {
+            if (repository is IEfCoreRepository<TEntity> efCoreRepository)
+            {
+                return efCoreRepository;
+            }
+
+            throw new ArgumentException("Given repository does not implement " + typeof(IEfCoreRepository<TEntity>).AssemblyQualifiedName, nameof(repository));
+        }
+    }
+}
---
+++ modules/identityserver/src/Volo.Abp.IdentityServer.Domain/Volo/Abp/IdentityServer/AbpClaimsService.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using IdentityServer4.Services;
+using Microsoft.Extensions.Logging;
+using Volo.Abp.Security.Claims;
+
+namespace Volo.Abp.IdentityServer
+{
+    public class AbpClaimsService : DefaultClaimsService
+    {
+        public AbpClaimsService(IProfileService profile, ILogger<DefaultClaimsService> logger)
+            : base(profile, logger)
+        {
+        }
+
+        protected override IEnumerable<string> FilterRequestedClaimTypes(IEnumerable<string> claimTypes)
+        {
+            return base.FilterRequestedClaimTypes(claimTypes).Union(new []{AbpClaimTypes.TenantId, AbpClaimTypes.EditionId});
+        }
+    }
+}
