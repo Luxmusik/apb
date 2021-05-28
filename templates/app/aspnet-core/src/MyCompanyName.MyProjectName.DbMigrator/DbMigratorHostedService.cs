c632984b8184a0472754daa1e47b1b12317ef783
Ali Mazloum <79036904+alisscco@users.noreply.github.com>
2021-05-28 09:38:33 +0300

    Initialize maliming/UserSecrets-Migrator

---
+++ templates/app/aspnet-core/src/MyCompanyName.MyProjectName.DbMigrator/DbMigratorHostedService.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using MyCompanyName.MyProjectName.Data;
+using Serilog;
+using Volo.Abp;
+
+namespace MyCompanyName.MyProjectName.DbMigrator
+{
+    public class DbMigratorHostedService : IHostedService
+    {
+        private readonly IHostApplicationLifetime _hostApplicationLifetime;
+        private readonly IServiceProvider _serviceProvider;
+
+        public DbMigratorHostedService(IHostApplicationLifetime hostApplicationLifetime, IServiceProvider serviceProvider)
+        {
+            _hostApplicationLifetime = hostApplicationLifetime;
+            _serviceProvider = serviceProvider;
+        }
+
+        public async Task StartAsync(CancellationToken cancellationToken)
+        {
+            using (var application = AbpApplicationFactory.Create<MyProjectNameDbMigratorModule>(options =>
+            {
+                options.UseAutofac();
+                options.Services.AddLogging(c => c.AddSerilog());
+                options.Configuration.EnvironmentName = _serviceProvider.GetRequiredService<IHostEnvironment>().EnvironmentName;
+                options.Configuration.UserSecretsAssembly = typeof(DbMigratorHostedService).Assembly;
+            }))
+            {
+                application.Initialize();
+
+                await application
+                    .ServiceProvider
+                    .GetRequiredService<MyProjectNameDbMigrationService>()
+                    .MigrateAsync();
+
+                application.Shutdown();
+
+                _hostApplicationLifetime.StopApplication();
+            }
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken) => Task.CompletedTask;
+    }
+}
