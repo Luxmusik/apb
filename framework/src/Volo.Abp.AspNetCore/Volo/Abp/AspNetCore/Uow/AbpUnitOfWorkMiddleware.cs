24c52ea9385b2ced3c769c086a4e5351d83cf20b
Ali Mazloum <79036904+alisscco@users.noreply.github.com>
2021-05-28 09:43:18 +0300

    Initialize maliming/OnModelCreating

---
+++ framework/src/Volo.Abp.AspNetCore/Volo/Abp/AspNetCore/Uow/AbpUnitOfWorkMiddleware.cs
@@ -0,0 +1,34 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Volo.Abp.DependencyInjection;
+using Volo.Abp.Uow;
+
+namespace Volo.Abp.AspNetCore.Uow
+{
+    public class AbpUnitOfWorkMiddleware : IMiddleware, ITransientDependency
+    {
+        private readonly IUnitOfWorkManager _unitOfWorkManager;
+
+        public AbpUnitOfWorkMiddleware(IUnitOfWorkManager unitOfWorkManager)
+        {
+            _unitOfWorkManager = unitOfWorkManager;
+        }
+
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            //TODO: Make this configurable.
+            if (context.Request.Path.Value != null &&
+                context.Request.Path.Value.StartsWith("/_blazor"))
+            {
+                await next(context);
+                return;
+            }
+            
+            using (var uow = _unitOfWorkManager.Reserve(UnitOfWork.UnitOfWorkReservationName))
+            {
+                await next(context);
+                await uow.CompleteAsync(context.RequestAborted);
+            }
+        }
+    }
+}
---
+++ modules/identity/src/Volo.Abp.Identity.EntityFrameworkCore/Volo/Abp/Identity/EntityFrameworkCore/IdentityModelBuilderConfigurationOptions.cs
@@ -0,0 +1,15 @@
+﻿using Volo.Abp.EntityFrameworkCore.Modeling;
+
+namespace Volo.Abp.Identity.EntityFrameworkCore
+{
+    public class IdentityModelBuilderConfigurationOptions : AbpModelBuilderConfigurationOptions
+    {
+        public IdentityModelBuilderConfigurationOptions()
+            : base(
+                AbpIdentityDbProperties.DbTablePrefix,
+                AbpIdentityDbProperties.DbSchema)
+        {
+
+        }
+    }
+}
