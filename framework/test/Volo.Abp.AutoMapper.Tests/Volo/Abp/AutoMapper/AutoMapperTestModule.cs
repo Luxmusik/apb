12d4d0be17f7f11863ea3c4fc727d390b23c527d
Ali Mazloum <79036904+alisscco@users.noreply.github.com>
2021-05-28 09:44:01 +0300

    Initialize 8261

---
+++ framework/test/Volo.Abp.AutoMapper.Tests/Volo/Abp/AutoMapper/AutoMapperTestModule.cs
@@ -0,0 +1,22 @@
+﻿using Volo.Abp.Modularity;
+using Volo.Abp.MultiLingualObject;
+using Volo.Abp.ObjectExtending;
+
+namespace Volo.Abp.AutoMapper
+{
+    [DependsOn(
+        typeof(AbpAutoMapperModule),
+        typeof(AbpObjectExtendingTestModule),
+        typeof(AbpMultiLingualObjectModule)
+    )]
+    public class AutoMapperTestModule : AbpModule
+    {
+        public override void ConfigureServices(ServiceConfigurationContext context)
+        {
+            Configure<AbpAutoMapperOptions>(options =>
+            {
+                options.AddMaps<AutoMapperTestModule>();
+            });
+        }
+    }
+}
